73f7fad95d9136d37fe52dc042c7d65ea49c08a4
{} <motose6@gmail.com>
2024-02-22 19:40:51 +0900

    up

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -1,25 +1,41 @@
 using EmptyBraces.Localization;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 namespace EmptyBraces
 {
 	public class Sample : MonoBehaviour
 	{
-		public SystemLanguage Language = SystemLanguage.English;
-		public TMProLocalize _tmVersion;
-
+		[SerializeField] TMProLocalize _tmVersion;
+		[SerializeField] Toggle[] _languageToggles;
 		void Awake()
 		{
-			LocalizationManager.LoadWordFile(Language);
+			LocalizationManager.LoadWordFile(SystemLanguage.English);
+			for (int i = 0; i < 3; ++i)
+			{
+				var icap = i;
+				_languageToggles[i].onValueChanged.AddListener(isOn =>
+				{
+					if (isOn)
+					{
+						OnLoadLanguage(icap switch {
+							0 => SystemLanguage.English,
+							1 => SystemLanguage.Japanese,
+							_ => SystemLanguage.ChineseSimplified,
+						});
+					}
+				});
+			}
 		}
 
 		void Start()
 		{
-			_tmVersion.SetDynamicMessage(() => Word.Get(LID.title_version, "1.0"));
+			_tmVersion.SetDynamicMessage(() => Word.Get(LID.title_version, PlayerSettings.bundleVersion));
 		}
 
 		public void OnLoadLanguage(SystemLanguage lan)
 		{
-			LocalizationManager.LoadWordFile(Language);
+			LocalizationManager.LoadWordFile(lan);
 			foreach (var i in FindObjectsByType<TMProLocalize>(FindObjectsSortMode.None))
 				i.RefreshText();
 		}
