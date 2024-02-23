ee33d1409300c8218ad7ce16ac4970008b2f295d
{} <motose6@gmail.com>
2024-02-23 22:59:39 +0900

    up

---
+++ Assets/upm/Samples~/Sample_Scene/Sample.cs
@@ -0,0 +1,43 @@
+using EmptyBraces.Localization;
+using UnityEngine;
+using UnityEngine.UI;
+namespace EmptyBraces
+{
+	public class Sample : MonoBehaviour
+	{
+		[SerializeField] TMProLocalize _tmVersion;
+		[SerializeField] Toggle[] _languageToggles;
+		[SerializeField] string _version = "1.0.0";
+		void Awake()
+		{
+			Word.LoadWordFile(SystemLanguage.English);
+			for (int i = 0; i < 3; ++i)
+			{
+				var icap = i;
+				_languageToggles[i].onValueChanged.AddListener(isOn =>
+				{
+					if (isOn)
+					{
+						OnLoadLanguage(icap switch {
+							0 => SystemLanguage.English,
+							1 => SystemLanguage.Japanese,
+							_ => SystemLanguage.ChineseSimplified,
+						});
+					}
+				});
+			}
+		}
+
+		void Start()
+		{
+			_tmVersion.SetDynamicMessage(() => Word.Get(LID.title_version, _version));
+		}
+
+		public void OnLoadLanguage(SystemLanguage lan)
+		{
+			Word.LoadWordFile(lan);
+			foreach (var i in FindObjectsByType<TMProLocalize>(FindObjectsSortMode.None))
+				i.RefreshText();
+		}
+	}
+}
